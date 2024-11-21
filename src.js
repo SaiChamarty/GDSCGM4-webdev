8ef16c20f297140456acb7c82420cabd76a4dec4
Sai Ganesh Chamarty <123051270+SaiChamarty@users.noreply.github.com>
2024-11-20 19:52:56 -0800

    Basic and Medium
    GDSC GM4 medium assignment

---
+++ src.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const button = document.getElementById('myButton');
+    
+    button.addEventListener('click', function() {
+        // Redirect to LinkedIn profile when the button is clicked
+        window.location.href = 'https://www.linkedin.com/in/sai-ganesh-chamarty-263342274/';
+    });
+});
